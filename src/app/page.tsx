da447f88387766e42531c0ea507fd7f947ee0d5c
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-11-04 20:46:39 +1100

    added photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import Link from "next/link";
+import Image from 'next/image';
+import homeImg from 'public/home.jpg'
 
 export default function Home() {
   return (
-    <div>
-      Home Page</div>
+    <div className='absolute -z-10 inset-0'>
+      Home Page
+      <Image src={homeImg} alt="Home Image" fill style={{objectFit: 'cover'}}
+      />
+    </div>
   );
 }
